ace6b3e147e7e2879c3695f9beec73c070a82a2d
tenklo <tenklo@pm.me>
2021-09-30 12:01:38 +0200

    emacs: set auth-source-save-behavior to nil

--- .emacs.d/custom.el
+++ .emacs.d/custom.el
@@ -3,6 +3,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(auth-source-save-behavior nil)
  '(bmkp-last-as-first-bookmark-file "~/.emacs.d/bookmarks")
  '(browse-url-browser-function 'eww-browse-url)
  '(browse-url-generic-program "o")
